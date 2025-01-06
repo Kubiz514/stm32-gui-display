7e3d8739d0144bffddfd44215ffd576c1c6b42cd
JakubPloch <kubaploch34@gmail.com>
2025-01-06 10:05:09 +0100

    Add watchdog

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
+#include "iwdg.h"
 #include "spi.h"
 #include "tim.h"
 #include "usart.h"
@@ -206,6 +207,7 @@ int main(void)
   MX_TIM1_Init();
   MX_TIM17_Init();
   MX_TIM16_Init();
+  MX_IWDG_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -276,6 +278,10 @@ int main(void)
 			finish_render_profiling();
 			lcd_copy_data();
 			start_lcd_data_transfer_profiling();
+
+			// watchdog
+			if(HAL_IWDG_Refresh(&hiwdg) != HAL_OK)
+				Error_Handler();
 		}
 
     /* USER CODE END WHILE */
@@ -304,7 +310,8 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
   RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
